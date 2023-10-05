1f0a62cc8849aa40c87f3619a8dff5b4c40a3460
Manuel Mo <manuelmo.steam@gmail.com>
2023-10-05 22:59:56 +0100

    Initial version

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,3 @@
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
@@ -11,7 +5,8 @@ public class Application {
     public static void main(String[] args) throws IOException {
         try {
             // Create a new Game object and call run()
-            Game game = new Game();
+            Game game;
+            game = new Game();
             game.run();
         } catch (IOException e) {
             // Handle or log the exception at the application level
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,24 +1,28 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
 public class Game {
     private Screen screen;
+    private Hero hero;
+    boolean flag;
 
     Game() throws IOException {
         try {
-            TerminalSize terminalSize = new TerminalSize(100, 20);
+            TerminalSize terminalSize = new TerminalSize(40, 20);
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Terminal terminal = terminalFactory.createTerminal();
             screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
-            screen.clear();
+            hero = new Hero(10, 10);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -26,7 +30,7 @@ public class Game {
     private void draw() throws IOException {
         try {
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            hero.draw(screen);
             screen.refresh();
         } catch (IOException e) {
             // Handle the exception or rethrow it as needed
@@ -35,10 +39,40 @@ public class Game {
     }
     public void run() throws IOException {
         try {
-            draw();
+            flag = true;
+            while (flag) {
+                draw();
+                KeyStroke key = screen.readInput();
+                processKey(key);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    private void processKey(KeyStroke key) throws IOException {
+        System.out.println(key);
+        switch(key.getKeyType()) {
+            case ArrowUp:
+                hero.moveUp();
+                break;
+            case ArrowDown:
+                hero.moveDown();
+                break;
+            case ArrowLeft:
+                hero.moveLeft();
+                break;
+            case ArrowRight:
+                hero.moveRight();
+                break;
+            case Character:
+                if(key.getCharacter() == 'q') {
+                    screen.close();
+                }
+                break;
+            case EOF:
+                flag = false;
+                break;
 
+        }
+    }
 }
