eb39ee03654d6c5183a9b544d7239dbe1d6dc48e
Manuel Mo <manuelmo.steam@gmail.com>
2023-10-05 21:01:30 +0100

    Initial version

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,27 +7,46 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 public class Game {
-
+    private Screen screen;
     Game() throws IOException {
         try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
             TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
-            TerminalScreen screen = new TerminalScreen(terminal);
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);terminal = terminalFactory.createTerminal();
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private void draw() {
-
+    private void draw() throws IOException {
+        try {
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
+        } catch (IOException e) {
+            // Handle the exception or rethrow it as needed
+            e.printStackTrace();
+        }
     }
-    public void run() {
-
+    public void run() throws IOException {
+        try {
+            draw();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        try {
+            // Create a new Game object and call run()
+            Game game = new Game();
+            game.run();
+        } catch (IOException e) {
+            // Handle or log the exception at the application level
+            e.printStackTrace();
+        }
     }
 }
